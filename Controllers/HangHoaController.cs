55150636b05f57795f4307b1548720fa4f3cc609
Your Name <you@example.com>
2024-08-17 20:06:21 +0700

    feat: danh sach hang hoa

--- Controllers/HangHoaController.cs
+++ Controllers/HangHoaController.cs
@@ -1,12 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EcommerceWeb.Data;
+using EcommerceWeb.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EcommerceWeb.Controllers
 {
     public class HangHoaController : Controller
     {
+        private readonly EcommerceDbContext _context;
+
+        public HangHoaController(EcommerceDbContext context) => _context = context;
+
         public IActionResult Index(int? loai)
         {
-            return View();
+            var hangHoas = _context.HangHoas.AsQueryable();
+
+            if (loai.HasValue)
+            {
+                hangHoas = hangHoas.Where(p => p.MaLoai == loai.Value);
+            }
+
+            var result = hangHoas.Select(p => new HangHoaVM
+            {
+                MaHh = p.MaHh,
+                TenHh = p.TenHh,
+                HinhAnh = p.Hinh ?? "",
+                MoTa = p.MoTaDonVi ?? "",
+                DonGia = p.DonGia ?? 0,
+                TenLoai = p.MaLoaiNavigation.TenLoai,
+            }).ToList();
+
+            return View(result);
+        }
+
+        public IActionResult Search(string? query)
+        {
+            var hangHoas = _context.HangHoas.AsQueryable();
+
+            if (query != null)
+            {
+                hangHoas = hangHoas.Where(p => p.TenHh.Contains(query));
+            }
+
+            var result = hangHoas.Select(p => new HangHoaVM
+            {
+                MaHh = p.MaHh,
+                TenHh = p.TenHh,
+                HinhAnh = p.Hinh ?? "",
+                MoTa = p.MoTaDonVi ?? "",
+                DonGia = p.DonGia ?? 0,
+                TenLoai = p.MaLoaiNavigation.TenLoai,
+            }).ToList();
+
+            return View(result);
         }
     }
 }
---
+++ ViewModels/HangHoaVM.cs
@@ -0,0 +1,12 @@
+﻿namespace EcommerceWeb.ViewModels
+{
+    public class HangHoaVM
+    {
+        public int MaHh { get; set; }
+        public string? TenHh { get; set; }
+        public string? HinhAnh { get; set; }
+        public string? MoTa { get; set; }
+        public double DonGia { get; set; }
+        public string? TenLoai { get; set; }
+    }
+}
